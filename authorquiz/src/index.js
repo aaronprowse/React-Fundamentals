3d0120c1120572d53319ae53df87337cebefc41c
aaronprowse <45644005+aaronprowse@users.noreply.github.com>
2020-08-28 23:48:01 +0100

    Events Module
    Completed the Events Modle of React Fundamentals

--- authorquiz/src/index.js
+++ authorquiz/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
 import {shuffle, sample} from 'underscore';
+import { render } from '@testing-library/react';
 
 const authors = [
   {
@@ -62,17 +63,23 @@ function getTurnData(authors) {
 }
 
 const state =  {
-  turnData: getTurnData(authors)
+  turnData: getTurnData(authors),
+  highlight: ''
 };
 
-ReactDOM.render(
-  <React.StrictMode>
-    <AuthorQuiz {...state} />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function onAnswerSelected(answer) {
+  const isCorrect = state.turnData.author.books.some((book) => book === answer);
+  state.highlight = isCorrect ? 'correct'  : 'wrong';
+  console.log(state.highlight);
+  renderScreen();
+}
+
+function renderScreen() {
+  ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />, document.getElementById('root'))
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+renderScreen();
 serviceWorker.unregister();
